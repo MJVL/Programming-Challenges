863bc330b53d9cb3e6a1621e243ff04c2eca46ad
Michael Van Leeuwen <michaeljvanleeuwen@gmail.com>
2018-03-29 12:33:04 -0400

    Add 0020 Java solution

---
+++ Hard Challenges/0020 - Procrastination Timer/Solution.java
@@ -0,0 +1,15 @@
+package Timer;
+
+import javax.swing.*;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        ScheduledExecutorService ses = Executors.newSingleThreadScheduledExecutor();
+        ses.scheduleAtFixedRate(() -> JOptionPane.showMessageDialog(null, "Get back to work!"), 0, 2, TimeUnit.HOURS);
+    }
+
+}
