d22b44f5ffac9633a8fb549e3a7d84158cfd96e7
Michael Van Leeuwen <michaeljvanleeuwen@gmail.com>
2018-05-28 19:20:20 -0400

    Add 0016 Python solution

---
+++ Easy Challenges/0016 - Character Removal/Solution.py
@@ -0,0 +1,14 @@
+
+def remove_characters(str_one, str_two):
+    for char in str_two:
+        str_one = str_one.replace(char, "")
+    return str_one
+
+
+def main():
+    str_one = str(input("Enter the string: "))
+    characters = str(input("Enter characters to remove: "))
+    print("Modified string: %s" % remove_characters(str_one, characters))
+
+if __name__ == '__main__':
+    main()
