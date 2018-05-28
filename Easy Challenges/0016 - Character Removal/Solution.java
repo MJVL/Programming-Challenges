5ded9b45ceb5fc622e280ddbea5b85f70acc6f3e
Michael Van Leeuwen <michaeljvanleeuwen@gmail.com>
2018-05-28 19:11:48 -0400

    Add 0016 Java solution

---
+++ Easy Challenges/0016 - Character Removal/Solution.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner reader = new Scanner(System.in);
+        System.out.print("Enter the string: ");
+        String strOne = reader.nextLine();
+        System.out.println("Enter the characters to remove:");
+        String strTwo = reader.nextLine();
+        System.out.println("Modified string: " + removeCharacters(strOne, strTwo));
+    }
+
+    public static String removeCharacters(String strOne, String strTwo) {
+        for (int i = 0; i < strTwo.length(); i++) {
+            strOne = strOne.replaceAll(strTwo.substring(i, i + 1), "");
+        }
+        return strOne;
+    }
+
+}
