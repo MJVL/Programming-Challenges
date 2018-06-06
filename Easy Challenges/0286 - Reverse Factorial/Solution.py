a6b098ba8c519fe91733b8207d2a138706ac0994
Michael Van Leeuwen <michaeljvanleeuwen@gmail.com>
2018-06-05 20:04:38 -0400

    Add 0286 Python solution

---
+++ Easy Challenges/0286 - Reverse Factorial/Solution.py
@@ -0,0 +1,16 @@
+def reverse_factorial(n, divisor):
+    if n / divisor == 1:
+        return n
+    return 0 if n % divisor > 0 else reverse_factorial(n / divisor, divisor + 1)
+
+
+def format_factorial(n):
+    return "None" if n == 0 else "%d!" % n
+
+
+def main():
+    num = int(input("Enter an integer: "))
+    print("%d = %s" % (num, format_factorial(reverse_factorial(num, 2))))
+
+if __name__ == '__main__':
+    main()
