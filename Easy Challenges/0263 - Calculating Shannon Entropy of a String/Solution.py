f9f205cc0b07246c4ece854124db2828ce73150e
Michael Van Leeuwen <michaeljvanleeuwen@gmail.com>
2018-06-06 21:26:59 -0400

    Rename parameter to fix shadowing issue

--- Easy Challenges/0263 - Calculating Shannon Entropy of a String/Solution.py
+++ Easy Challenges/0263 - Calculating Shannon Entropy of a String/Solution.py
@@ -1,8 +1,8 @@
 from math import log2
 
 
-def shannon_entropy(str):
-    return -sum(str.count(l) / len(str) * log2(str.count(l) / len(str)) for l in set(str))
+def shannon_entropy(seq):
+    return -sum(seq.count(l) / len(seq) * log2(seq.count(l) / len(seq)) for l in set(seq))
 
 
 def main():
