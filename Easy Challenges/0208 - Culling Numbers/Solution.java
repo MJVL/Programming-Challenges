02ef937fd06e7c68218d6071d16f804a7896ef7a
Michael Van Leeuwen <michaeljvanleeuwen@gmail.com>
2018-06-07 09:23:01 -0400

    Compress stream

--- Easy Challenges/0208 - Culling Numbers/Solution.java
+++ Easy Challenges/0208 - Culling Numbers/Solution.java
@@ -13,8 +13,7 @@ public class Solution {
 
     public static Set<Integer> cullNumbers(int[] arrNums) {
        Set<Integer> setUnique = new HashSet<Integer>();
-       Arrays.stream(arrNums)
-           .forEach(setUnique::add);
+       Arrays.stream(arrNums).forEach(setUnique::add);
        return setUnique;
     }
 
