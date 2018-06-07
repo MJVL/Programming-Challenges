5cdd4dfba1063042e7a8eddcb1f79ddc144a4346
Michael Van Leeuwen <michaeljvanleeuwen@gmail.com>
2018-06-06 20:09:06 -0400

    Add 0263 Java solution

---
+++ Easy Challenges/0263 - Calculating Shannon Entropy of a String/Solution.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+import java.util.Map;
+import java.util.HashMap;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner reader = new Scanner(System.in);
+        System.out.print("Enter sequence: ");
+        System.out.printf("%.5f", shannonEntropy(reader.nextLine()));
+    }
+
+    public static double shannonEntropy(String str) {
+        Map<Character, Integer> mapSymbol = new HashMap<Character, Integer>();
+        for (char c: str.toCharArray()) {
+            mapSymbol.put(c, !mapSymbol.containsKey(c) ? 1 : mapSymbol.get(c) + 1);
+        }
+        double sEntropy = 0.0;
+        for (char c: mapSymbol.keySet()) {
+            sEntropy -= (double) mapSymbol.get(c) / str.length() * Math.log((double) mapSymbol.get(c) / str.length()) / Math.log(2);
+        }
+        return sEntropy;
+    }
+}
