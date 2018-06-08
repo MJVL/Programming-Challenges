fb506aac18965623355dcbb9847515695ed2ea31
Michael Van Leeuwen <michaeljvanleeuwen@gmail.com>
2018-06-08 17:43:51 -0400

    Add 0140 Python solution

---
+++ Easy Challenges/0140 - Variable Notation/Solution.py
@@ -0,0 +1,19 @@
+def to_camel(series):
+    return series[0].lower() + ''.join(s for s in series.title() if s.isalpha())[1:]
+
+
+def to_snake(series):
+    return series.replace(" ", "_").lower()
+
+
+def to_upper_snake(series):
+    return series.replace(" ", "_").upper()
+
+
+def main():
+    mode = int(input("Enter mode (0 - Camel Case, 1 - Snake Case, 2 - Capitalized Snake Case): "))
+    series = str(input("Enter series: "))
+    print(to_camel(series) if mode == 0 else (to_snake(series) if mode == 1 else (to_upper_snake(series))))
+
+if __name__ == '__main__':
+    main()
