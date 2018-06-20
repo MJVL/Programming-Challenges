0ad5176226a64f1d17fbd0b2e3737f1a07206388
Michael Van Leeuwen <michaeljvanleeuwen@gmail.com>
2018-06-19 22:10:53 -0400

    Add base case to conditional operator

--- Easy Challenges/0296 - The Twelve Days of Christmas/Solution.java
+++ Easy Challenges/0296 - The Twelve Days of Christmas/Solution.java
@@ -10,10 +10,7 @@ public class Solution {
     }
 
     public static String twelveDaysOfChristmas(int day) {
-        if (day >= GIFTS.length) {
-            return "";
-        }
-        return "On the " + ordinal(day + 1) + " day of Christmas\nmy true love sent to me:\n" + ((day > 0) ? (Arrays.asList(GIFTS).subList(GIFTS.length - day - 1, GIFTS.length)).stream().map(s -> s + "\n").collect(Collectors.joining()) : GIFTS[GIFTS.length - 1].substring(GIFTS[GIFTS.length - 1].indexOf(" ") + 1) + "\n") + "\n" + twelveDaysOfChristmas(day + 1);
+        return (day >= GIFTS.length) ? "" : "On the " + ordinal(day + 1) + " day of Christmas\nmy true love sent to me:\n" + ((day > 0) ? (Arrays.asList(GIFTS).subList(GIFTS.length - day - 1, GIFTS.length)).stream().map(s -> s + "\n").collect(Collectors.joining()) : GIFTS[GIFTS.length - 1].substring(GIFTS[GIFTS.length - 1].indexOf(" ") + 1) + "\n") + "\n" + twelveDaysOfChristmas(day + 1);
     }
 
     public static String ordinal(int i) {
