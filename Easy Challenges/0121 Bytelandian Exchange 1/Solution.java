8db1a7182c5254b2746f381a82850e2f5bb67fad
Michael Van Leeuwen <michaeljvanleeuwen@gmail.com>
2018-06-23 22:36:28 -0400

    Add 0121 Java solution

---
+++ Easy Challenges/0121 Bytelandian Exchange 1/Solution.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner reader = new Scanner(System.in);
+        System.out.print("Enter the coin value: ");
+        System.out.println(exchange(reader.nextInt()));
+    }
+
+    public static int exchange(int n) {
+        return (n == 0) ? 1 : exchange(n / 2) + exchange(n / 3) + exchange(n / 4);
+    }
+    
+}
