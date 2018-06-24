8a82c446c8f606e460df0245c5c3b41aa2ea2514
Michael Van Leeuwen <michaeljvanleeuwen@gmail.com>
2018-06-24 09:02:56 -0400

    Refactor distance functions

--- Easy Challenges/0259 - Clarence the Slow Typist/Solution.py
+++ Easy Challenges/0259 - Clarence the Slow Typist/Solution.py
@@ -1,9 +1,9 @@
 def total_distance(inputs):
-    return sum(distance(keyboard_coordinate(i)[1] - keyboard_coordinate(j)[1], keyboard_coordinate(i)[0] - keyboard_coordinate(j)[0]) for i, j in zip(inputs, inputs[1:]))
+    return sum(distance(keyboard_coordinate(i), keyboard_coordinate(j)) for i, j in zip(inputs, inputs[1:]))
 
 
 def distance(a, b):
-    return (a ** 2 + b ** 2) ** .5
+    return ((a[0] - b[0]) ** 2 + (a[1] - b[1]) ** 2) ** .5
 
 
 def keyboard_coordinate(c):
